7562c1080267b602ff866c0663c439268d8effb4
mesu1gok <mesutcukgok@gmail.com>
2025-12-08 18:14:19 +0200

    Comment out original SQL queries in sql_assignment2.sql
    Commented out original SQL queries for clarity.

--- assignment2/sql_assignment2.sql
+++ assignment2/sql_assignment2.sql
@@ -1,16 +1,12 @@
 -- Assignment 2: DVDRental Queries
 -- Description: Basic PostgreSQL queries for Patika.dev course
 
--- 1) Show all columns from film table where replacement_cost is between 12.99 and 16.99
-SELECT * FROM film
+-- 1) SELECT * FROM film
 WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 
--- 2) Show first_name and last_name from actor table for actors 'Penelope', 'Nick', or 'Ed'
-SELECT first_name, last_name FROM actor
+-- 2) SELECT first_name, last_name FROM actor
 WHERE first_name IN ('Penelope','Nick','Ed');
 
--- 3) Show all columns from film table where rental_rate is 0.99, 2.99, or 4.99 
--- AND replacement_cost is 12.99, 15.99, or 28.99
-SELECT * FROM film
+-- 3) SELECT * FROM film
 WHERE rental_rate IN (0.99,2.99,4.99)
 AND replacement_cost IN (12.99,15.99,28.99);
